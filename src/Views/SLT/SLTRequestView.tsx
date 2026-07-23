d07186da7fe6f03bdbe5b6e4e668b2f570ab21d4
SamridhManzeera <Samridh@manzeerasolutions.co.uk>
2026-07-23 17:07:56 +0530

    feat:UI table dark

--- src/Views/SLT/SLTRequestView.tsx
+++ src/Views/SLT/SLTRequestView.tsx
@@ -47,14 +47,6 @@ function CalendarSmallIcon() {
   );
 }
 
-function InfoIcon() {
-  return (
-    <svg width="13" height="13" viewBox="0 0 24 24" fill="currentColor">
-      <path d="M11 7h2v2h-2V7zm0 4h2v6h-2v-6zm1-9C6.48 2 2 6.48 2 12s4.48 10 10 10 10-4.48 10-10S17.52 2 12 2zm0 18c-4.41 0-8-3.59-8-8s3.59-8 8-8 8 3.59 8 8-3.59 8-8 8z" />
-    </svg>
-  );
-}
-
 function FileIcon() {
   return (
     <svg
@@ -360,7 +352,6 @@ export default function SLTRequestView() {
                             data-tooltip-content={slotBreakdownText(breakdown)}
                           >
                             {requested}
-                            <InfoIcon />
                           </span>
                         </td>
                         <td className="slv__alloc-cell">
